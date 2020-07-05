e60d4aec1797e2103c94c226db57a7e7b290ea1a
boyhendrawan <boyhendrawanpurba@gmail.com>
2020-07-05 19:49:23 +0700

    CURD AND FOTO ERD

--- resources/views/Tugas/create.blade.php
+++ resources/views/Tugas/create.blade.php
@@ -15,15 +15,15 @@ Created Artikel
 			@csrf
 		  <div class="form-group">
 		    <label for="exampleFormControlInput1">Judul</label>
-		    <input type="text" class="form-control" name="judul" placeholder="Tulisankan Judul">
+		    <input type="text" class="form-control" name="judul" placeholder="Tulisankan Judul" required>
 		  </div>
 		  <div class="form-group">
 		    <label for="exampleFormControlInput1">Tag</label>
-		    <input type="text" class="form-control" name="tag" placeholder="Tulisankan Tag">
+		    <input type="text" class="form-control" name="tag" placeholder="Tulisankan Tag" required>
 		  </div>
 		  <div class="form-group">
 		    <label for="exampleFormControlTextarea1">Artikel</label>
-		    <textarea class="form-control" name="isi" id="exampleFormControlTextarea1" rows="3"></textarea>
+		    <textarea class="form-control" name="isi" id="exampleFormControlTextarea1" required rows="3"></textarea>
 		  </div>
 		  <button class="btn btn-success">Kirim</button>
 		  <a href="{{url('/artikel')}}" class="btn btn-danger">Back</a>
